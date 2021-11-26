f4f067753c85a7e64bf56dd5c575645bfe386bd1
Antran2598 <95039474+Antran2598@users.noreply.github.com>
2021-11-26 15:00:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace NV_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(6969), false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(NV_BUS), "XXX", WellKnownObjectMode.SingleCall);
+            RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off; // for DEBUG
+            Console.WriteLine("SERVER is started ..."); 
+            Console.Read();
+        }
+    }
+}
